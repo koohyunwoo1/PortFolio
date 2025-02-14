489b50e58243f41b51646322306b007e35312020
구현우 <kjklovekhw@gmail.com>
2025-02-14 16:49:17 +0900

    포트폴리오 라이트모드/다크모드 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,25 @@
-import "./App.css";
+import { useState, useEffect } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "../src/pages/Home/Home";
 import Project from "../src/pages/Project/Project";
 import Contact from "../src/pages/Contact/Contact";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import "./App.css";
 
 function App() {
+  const [theme, setTheme] = useState("dark");
+
+  useEffect(() => {
+    document.documentElement.setAttribute("data-theme", theme);
+  }, [theme]);
+
   return (
     <>
+      <button onClick={() => setTheme(theme === "dark" ? "light" : "dark")}>
+        {theme === "dark" ? "라이트 모드로 변경" : "다크 모드로 변경"}
+      </button>
+
       <Router>
         <Routes>
           <Route path="/" element={<Home />} />
